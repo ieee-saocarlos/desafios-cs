db07305bfa0a8dc2161b6af92e106ae0328162ae
Lucas Witzel <lucaswitzelferreira@gmail.com>
2020-06-03 23:37:11 -0300

    produtos.py

---
+++ Lucas Witzel/produtos.py
@@ -0,0 +1,13 @@
+leite=int(input())
+ovo=int(input())
+prendedores=int(input())
+sabao=int(input())
+iogurte=int(input())
+
+l = 12 * leite
+o = 12 * ovo
+p = 24 * prendedores
+s = 5 * sabao
+i = 6 * iogurte
+
+print("Há {0} caixas de leite, {1} ovos, {2} prendedores, {3} barras de sabão e {4} copinhos de iogurte".format(l,o,p,s,i))
