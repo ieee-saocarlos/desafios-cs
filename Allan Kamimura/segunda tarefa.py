40bf8a12580b908a80efdf8f9f9c9bced46ce7a2
Allan Kamimura <allan_kamimura@hotmail.com>
2020-06-05 01:01:16 -0300

    adicionada a lista de compras

---
+++ Allan Kamimura/segunda tarefa.py
@@ -0,0 +1,26 @@
+Leite = 12
+while Leite < 936:
+        print("leite:", Leite)
+        Leite += 1
+Ovo = 12
+while Ovo < 816:
+        print("ovos:", Ovo)
+        Ovo += 1
+Prendedor = 24
+while Prendedor < 504:
+    print("prendedores:" , Prendedor)
+    Prendedor += 1
+Barras_de_Sabão = 5
+while Barras_de_Sabão < 65:
+    print("barras de sabão:", Barras_de_Sabão)
+    Barras_de_Sabão  += 1
+print("pq leite nao ta no plural?")
+Copinho_de_Iogurte = 6
+while Copinho_de_Iogurte < 240:
+    print("copinhos de iogurte:", Copinho_de_Iogurte)
+    Copinho_de_Iogurte += 1
+print("leite:", Leite)
+print("ovos:", Ovo)
+print("prendedores:" , Prendedor)
+print("barras de sabão:", Barras_de_Sabão)
+print("copinhos de iogurte:", Copinho_de_Iogurte)
