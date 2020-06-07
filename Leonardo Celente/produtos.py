2de154503ca1bcb72cf23266e994e77385f399c1
Leonardo Celente <leocelente@gmail.com>
2020-06-07 02:28:46 -0300

    produtos.py tarefa 2

---
+++ Leonardo Celente/produtos.py
@@ -0,0 +1,13 @@
+'''
+Leite 	        12 	78
+Ovo 	        12 	68
+Prendedores 	24 	21
+Sabão 	        5 	13
+Iogurte 	    6 	40
+'''
+
+print("""leite: 936
+ovos: 816
+prendedores: 504
+barras de sabão: 65
+copinhos de iogurte: 240""")
