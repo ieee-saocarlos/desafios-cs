49ead73fb071187fb1d3b8d125064ddce2110be2
Leonardo Celente <leocelente@gmail.com>
2020-06-07 02:39:43 -0300

    produtos.py tarefa 3

--- Leonardo Celente/produtos.py
+++ Leonardo Celente/produtos.py
@@ -5,9 +5,22 @@ Prendedores 	24 	21
 Sabão 	        5 	13
 Iogurte 	    6 	40
 '''
+c_leite = 12,
+c_ovo = 12,
+c_prend = 24,
+c_sabão = 5,
+c_iogurte = 6
 
-print("""leite: 936
-ovos: 816
-prendedores: 504
-barras de sabão: 65
-copinhos de iogurte: 240""")
+leite = input("Conjuntos de Leite")
+ovo = input("Conjuntos de Ovo")
+prend = input("Conjuntos de Prendedores")
+sabao = input("Conjuntos de Sabão")
+iogur = input("Conjuntos de Iogurte")
+
+
+print("Há {} caixas de leite, {} ovos, {} prendedores, {} barras de sabão e {} copinhos de iogurte".format(\
+    leite*c_leite,\
+    ovo*c_ovo,\
+    prend*c_prend,\
+    sabao*c_sabão,\
+    iogur*c_iogurte))
