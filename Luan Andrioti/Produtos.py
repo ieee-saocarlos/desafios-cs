0b64476eba21d3948d50ce847203e067f5b0b3fd
Luan <luanfelipe@usp.br>
2020-06-08 00:59:53 -0300

    Produtos

---
+++ Luan Andrioti/Produtos.py
@@ -0,0 +1,29 @@
+leite = int(input("Digite a quantidade de conjuntos de leite: "))
+ovo = int(input("Digite a quantidade de conjuntos de ovos: "))
+prendedores = int(input("Digite a quantidade de conjuntos de prendedores: "))
+sabão = int(input("Digite a quantidade de conjuntos de sabão: "))
+iogurte = int(input("Digite a quantidade de conjuntos de iogurte: "))
+
+a = 12*leite
+b = 12*ovo
+c = 24*prendedores
+d = 5*sabão
+e = 6*iogurte
+
+print("Há", a,"caixas de leite,",b,"ovos,",c,"prendedores,",d,"barras de sabão e",e,"copinhos de iogurte")
+
+print("Há {0} caixas de leite, {1} ovos, {2} prendedores, {3} barras de sabão e {4} copinhos de iogurte".format(a, b, c, d, e))
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
