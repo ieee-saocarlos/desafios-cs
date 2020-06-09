a5bf6f386d75705faa05c5df5fdac0d5f36c5ade
Leonardo Celente <leocelente@gmail.com>
2020-06-08 21:53:42 -0300

    tarefa 3 correta

--- Leonardo Celente/produtos.py
+++ Leonardo Celente/produtos.py
@@ -5,22 +5,17 @@ Prendedores 	24 	21
 Sabão 	        5 	13
 Iogurte 	    6 	40
 '''
-c_leite = 12,
-c_ovo = 12,
-c_prend = 24,
-c_sabão = 5,
+c_leite = 12
+c_ovo = 12
+c_prend = 24
+c_sabão = 5
 c_iogurte = 6
 
-leite = input("Conjuntos de Leite")
-ovo = input("Conjuntos de Ovo")
-prend = input("Conjuntos de Prendedores")
-sabao = input("Conjuntos de Sabão")
-iogur = input("Conjuntos de Iogurte")
+leite = int(input("Conjuntos de Leite"))
+ovo = int(input("Conjuntos de Ovo"))
+prend = int(input("Conjuntos de Prendedores"))
+sabao = int(input("Conjuntos de Sabão"))
+iogur = int(input("Conjuntos de Iogurte"))
 
 
-print("Há {} caixas de leite, {} ovos, {} prendedores, {} barras de sabão e {} copinhos de iogurte".format(\
-    leite*c_leite,\
-    ovo*c_ovo,\
-    prend*c_prend,\
-    sabao*c_sabão,\
-    iogur*c_iogurte))
+print("Há {} caixas de leite, {} ovos, {} prendedores, {} barras de sabão e {} copinhos de iogurte".format(leite*c_leite,ovo*c_ovo,prend*c_prend,sabao*c_sabão,iogur*c_iogurte))
