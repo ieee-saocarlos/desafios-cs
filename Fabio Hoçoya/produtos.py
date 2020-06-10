bf0ddfbd20f24f94a9c517378160dd4d8e1cb927
Fabio Naoto <fabio.naoto.mh@outlook.com>
2020-06-09 21:15:09 -0300

    adicionado 'produtos'

---
+++ Fabio Hoçoya/produtos.py
@@ -0,0 +1,13 @@
+L = int (input ("Quantidade de conjuntos: "))
+O = int (input ("Quantidade de conjuntos: "))
+P = int (input ("Quantidade de conjuntos: "))
+S = int (input ("Quantidade de conjuntos: "))
+I = int (input ("Quantidade de conjuntos: "))
+
+L = 12*L
+O = 12*O
+P = 24*P
+S = 5*S
+I = 6*I
+
+print("Há", L, "caixas de leite,", O, "ovos,", P, "prendedores,", S, "barras de sabão e", I, "copinhos de iogurte")
